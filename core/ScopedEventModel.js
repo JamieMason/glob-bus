a95b97c23d9a056d96c156367d5148c380912afd
Jamie Mason <jamie.mason@bskyb.com>
2011-03-18 16:49:36 +0000

    Refactoring of ScopedEventModel

--- core/ScopedEventModel.js
+++ core/ScopedEventModel.js
@@ -1,118 +1,120 @@
-
-function scopedEventModel(subject, that)
+var ScopedEventModel = (function()
 {
-	that = that || {};
-	subject = subject || that;
+	// Model helpers
+	// ==================================================================
 	
-	function _proxy(namespaceString, callback, excludeCapturingScopes)
+	// Note - these functions apply to specific scopes
+	// ie: getting "type:this.that.other" would not include anything stored
+	// at "type:this.that" or "type:this"   
+
+	// oModel.get
+	function getShallow (oSelf, oModel, sScopeChain)
 	{
-		if (typeof namespaceString !== 'string')
-		{
-			return null;
-		}
-
-		var nsNames = namespaceString.split(':'),
-
-			// store this to save looking it up on every pass of the loop
-		    nsNamesLength = nsNames.length,
-
-			eventType = '',
-
-			scopeChain = '';
-
-		if (nsNamesLength < 1 || nsNamesLength > 2)
-		{
-			return null;
-		}
-
-		// portion before the : is the event type
-		eventType = nsNames[0];
-
-		// if selector is not global
-		if (eventType !== '*')
-		{
-			// and only an event type was sent, the scope is 'event:*'
-			scopeChain = nsNamesLength === 2 ? nsNames[1] : '*';
-		}
-
-		return callback(eventType, scopeChain, excludeCapturingScopes);
+		return (sScopeChain in oModel ? oModel[sScopeChain] : null);
 	}
 
-	function _set(eventType, scopeChain)
+	function getDeep (oSelf, oModel, sScopeChain)
 	{
-		// if items should be global, ie applying to all types at all scopes
-		if (eventType === '*')
+		var sKey,
+		    regexp = new RegExp('^' + sScopeChain + '.+'), 
+		    aMatchingScopes = [];
+		
+		for (sKey in oModel)
 		{
-			// store items there
-			subject[eventType] = subject[eventType] || [];
 			
-			// return reference to callee
-			return subject[eventType];
 		}
-		
-		// else items should be stored against a type
-
-		// create event type if undefined
-		subject[eventType] = subject[eventType] || {};
-
-		// add scope chain on event type if undefined
-		subject[eventType][scopeChain] = subject[eventType][scopeChain] || [];
-
-		// return reference to callee
-		return subject[eventType][scopeChain];
 	}
 
-	function _get(kind, scopeChain, excludeCapturingScopes)
+	// oModel.set
+	function set (oSelf, oModel, sScopeChain)
 	{
-		var itemsStoredInThisScope = [],
-		    scopesArray = subject[kind] || [],
-			globalArray = subject['*'] || [],
-		    scope;
+		return (oModel[sScopeChain] = sScopeChain in oModel ? oModel[sScopeChain] : []);
+	}
 
-		if (scopesArray.length === 0 && globalArray.length === 0)
-		{
-			return null;
-		}
-		
-		// if we only want eg 'ui:betslip.login.open' and not eg 'ui:betslip.login' or 'ui:betslip' 
-		if (excludeCapturingScopes)
-		{
-			return typeof scopesArray[scopeChain] !== 'undefined' ? scopesArray[scopeChain] : null;
-		}
+	function isSet (oSelf, oModel, sScopeChain)
+	{
+		return sScopeChain in oModel && 'length' in oModel[sScopeChain];
+	}
+	
+	function isEmpty (oSelf, oModel, sScopeChain)
+	{
+		return oSelf.isSet(sScopeChain) && oModel[sScopeChain].length === 0;
+	}
 
-		// otherwise we want to include everything interested in our event (recommended)
-		for (scope in scopesArray)
+	function containsItem (oSelf, oModel, sScopeChain, mItem)
+	{
+		if (oSelf.isSet(sScopeChain))
 		{
-			// if these items are related to all items of this kind
-			// OR the scope for these items matches ours exactly (eg scope is 'betslip.login.open' and the items here are scoped to 'betslip.login.open' too)
-			// OR our scope falls within the broader scope these items are related to (eg scope is 'betslip.login.open' and the items here are scoped to 'betslip')
-			if (scopesArray.hasOwnProperty(scope) && (scopeChain.replace(scope, '') === '' || scopeChain.replace(scope, '').charAt(0) === '.'))
+			for (var i = 0; i < oModel[sScopeChain].length; i++)
 			{
-				// add these handlers to our array
-				itemsStoredInThisScope = itemsStoredInThisScope.concat(scopesArray[scope]);
+				if (oModel[sScopeChain][i] === mItem)
+				{
+					return true;
+				}
 			}
 		}
-		
-		// add globals if set
-		if (globalArray.length !== 0)
-		{
-			itemsStoredInThisScope = itemsStoredInThisScope.concat(globalArray);
-		}
-
-		// send back the handlers
-		return itemsStoredInThisScope;
+				
+		return false;
 	}
 
-	that.set = function (selector)
+	// oModel.set
+	function addItem (oSelf, oModel, sScopeChain, mItem)
 	{
-		return _proxy(selector, _set);
-	};
+		if (oSelf.containsItem(sScopeChain, mItem))
+		{
+			return false;
+		}
+		
+		if (!oSelf.isSet(sScopeChain))
+		{
+			oSelf.set(sScopeChain);
+		}
+		
+		oModel[sScopeChain].push(mItem);
+	}
 
-	that.get = function (selector, excludeCapturingScopes)
+	// oModel.removeIfEmpty
+	function removeIfEmpty (oSelf, oModel, sScopeChain)
 	{
-		return _proxy(selector, _get, excludeCapturingScopes === true);
-	};
+		if (oSelf.isEmpty(sScopeChain))
+		{
+			delete oModel[sScopeChain];
+		}
+	}
 	
-	return that;
-}
+	function curry (fn /*, ... */)
+	{
+		var aCurryArgs = Array.prototype.slice.call(arguments, 1);
 
+		return function ( /* ... */ )
+		{
+			var aCalleeArgs = Array.prototype.slice.call(arguments, 0),
+				aMergedArgs = aCurryArgs.concat(aCalleeArgs);
+
+			return fn.apply(this, aMergedArgs);
+		};
+	}
+
+
+
+	// Exposed Constructor 
+	// ==================================================================
+	
+	function ScopedEventModel ()
+	{
+		var oModel = {};
+
+		this.getShallow = curry(getShallow, this, oModel);
+		this.getDeep = curry(getDeep, this, oModel);
+		this.set = curry(set, this, oModel);
+		this.isSet = curry(isSet, this, oModel);
+		this.isEmpty = curry(isEmpty, this, oModel);
+		this.containsItem = curry(containsItem, this, oModel);
+		this.addItem = curry(addItem, this, oModel);
+		this.removeIfEmpty = curry(removeIfEmpty, this, oModel);
+	}
+	
+	// return constructor to outer scope
+	return ScopedEventModel;
+	
+})();
